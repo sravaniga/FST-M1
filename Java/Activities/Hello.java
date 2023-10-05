2e963b2713c9a1f42b0fb259020601ea95ec0719
sravaniga <147052296+sravaniga@users.noreply.github.com>
2023-10-05 16:25:20 +0530

    Add files via upload

---
+++ Java/Activities/Hello.java
@@ -0,0 +1,12 @@
+public class Hello {
+
+        public static void main(String[] args) {
+
+            System.out.println("Hello, World!");
+int mynum=10;
+mynum=20;
+            System.out.println("mynum is"+mynum );
+        }
+
+    }
+
